06bf06bd40964095ac8a1844007a7358bcc8daf0
mdietrich97 <47761620+mdietrich97@users.noreply.github.com>
2019-02-21 12:10:25 +0100

    Update Sunrise.java

--- src/Sunrise.java
+++ src/Sunrise.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Sunrise {
+    Results results;
+    String status;
+    public class Results{
+        String sunrise;
+        String sunset;
+        String day_length;
+    }
 }
